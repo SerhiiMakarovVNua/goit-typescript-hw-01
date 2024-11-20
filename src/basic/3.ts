2dde9e467fbc7e1c255420088ca79ddbe37dee8e
SerhiiMakarovVNua <serhiimakarov79@gmail.com>
2024-11-20 11:30:08 +0200

    ts-hw-01.01

---
+++ src/basic/3.ts
@@ -0,0 +1,3 @@
+let value: string | number;
+
+let userStatus: 'enable' | 'disable';
---
+++ src/basic/6.ts
@@ -0,0 +1,25 @@
+interface UserProfile {
+  name: string;
+  age: number;
+  email: string;
+  address?: {
+    city: string;
+    country: string;
+  };
+}
+
+const mango: UserProfile = {
+  name: 'Mango',
+  age: 30,
+  email: 'john@example.com',
+  address: {
+    city: 'New York',
+    country: 'USA',
+  },
+};
+
+const poly: UserProfile = {
+  name: 'Poly',
+  age: 30,
+  email: 'poly@example.com',
+};
